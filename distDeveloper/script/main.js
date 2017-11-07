fcd882bc3e26874d8c80c4a7b72d4bb8be15a26d
MrHidalgo <Luckyhidalgo@gmail.com>
2017-11-08 00:03:00 +0200

    Small change and fix bug. (hor scroll i safari, padding contacts section, script offset)

--- distDeveloper/script/main.js
+++ distDeveloper/script/main.js
@@ -10,104 +10,123 @@
  * @description
  */
 function pushOffsetValue(arrElem, arr) {
-	arrElem.each(function (idx, item) {
-		arr.push(parseInt($(item).css("left")));
-	});
+    arrElem.each(function (idx, item) {
+        arr.push(parseInt($(item).css("left")));
+    });
+}
+
+/**
+ * @name listenerWindowModify
+ * @function
+ *
+ * @param arrLeftImg
+ * @param arrRightImg
+ *
+ * @description
+ */
+function listenerWindowModify(arrLeftImg, arrRightImg) {
+    $(window).on("change resize load", function () {
+        var mainLargeImage = $(".mainLarge_right"),
+            mainMediumImage = $(".mainMedium_right"),
+            imgRight = $(".resize_right"),
+            imgLeft = $(".resize_left"),
+            imgLeftSmooth = $('.resize_left-s'),
+            imgRightSmooth = $('.resize_right-s');
+
+        var WINWIDTH_LARGE = 1920,
+            WINWIDTH_MEDIUM = 1366;
+
+        var widthWindow = $(this).width(),
+            offsetEl = (WINWIDTH_LARGE - widthWindow) / 2,
+            offsetMediumEl = (WINWIDTH_MEDIUM - widthWindow) / 2;
+
+        console.log("widthWindow: ", widthWindow);
+
+        // BIG DEVICE
+        if (widthWindow > '1279' && widthWindow < WINWIDTH_LARGE) {
+
+            mainLargeImage.each(function (idx, item) {
+                $(item).css({
+                    right: -offsetEl
+                });
+            });
+        } else {
+            mainLargeImage.removeAttr("style");
+        }
+
+        // MEDIUM DEVICE
+        if (widthWindow > '767' && widthWindow < WINWIDTH_MEDIUM) {
+            var widthOffset = 0;
+
+            if (widthWindow > '767' && widthWindow < '992') {
+                widthOffset = 150;
+            } else {
+                widthOffset = 50;
+            }
+
+            mainMediumImage.each(function (idx, item) {
+                $(item).css({
+                    right: -(offsetMediumEl - widthOffset)
+                });
+            });
+        } else {
+            mainMediumImage.removeAttr("style");
+        }
+
+        // OTHER IMAGE
+        if (widthWindow > '290' && widthWindow < WINWIDTH_LARGE) {
+
+            imgLeft.each(function (idx, item) {
+                $(item).css({
+                    left: -offsetEl
+                });
+            });
+
+            imgRight.each(function (idx, item) {
+                $(item).css({
+                    right: -offsetEl
+                });
+            });
+
+            imgLeftSmooth.each(function (idx, item) {
+                $(item).css({
+                    left: arrLeftImg[idx] - offsetEl
+                });
+            });
+
+            imgRightSmooth.each(function (idx, item) {
+                $(item).css({
+                    left: arrRightImg[idx] - offsetEl
+                });
+            });
+        } else {
+            imgLeft.removeAttr("style");
+            imgRight.removeAttr("style");
+            imgLeftSmooth.removeAttr("style");
+            imgRightSmooth.removeAttr("style");
+        }
+    });
 }
 
 /**
  *
  */
-$(window).on("change resize load", function () {
-	var arrLeftSmoothImage = [],
-	    arrRightSmoothImage = [];
-
-	var mainLargeImage = $(".mainLarge_right"),
-	    mainMediumImage = $(".mainMedium_right"),
-	    imgRight = $(".resize_right"),
-	    imgLeft = $(".resize_left"),
-	    imgLeftSmooth = $('.resize_left-s'),
-	    imgRightSmooth = $('.resize_right-s');
-
-	pushOffsetValue(imgLeftSmooth, arrLeftSmoothImage);
-	pushOffsetValue(imgRightSmooth, arrRightSmoothImage);
-
-	var WINWIDTH_LARGE = 1920,
-	    WINWIDTH_MEDIUM = 1366;
-
-	var widthWindow = $(this).width(),
-	    offsetEl = (WINWIDTH_LARGE - widthWindow) / 2,
-	    offsetMediumEl = (WINWIDTH_MEDIUM - widthWindow) / 2;
-
-	// BIG DEVICE
-	if (widthWindow > '1348' && widthWindow < WINWIDTH_LARGE) {
-
-		mainLargeImage.each(function (idx, item) {
-			$(item).css({
-				right: -offsetEl
-			});
-		});
-	} else {
-		mainLargeImage.removeAttr("style");
-	}
-
-	// MEDIUM DEVICE
-	if (widthWindow > '767' && widthWindow < WINWIDTH_MEDIUM) {
-		var widthOffset = 0;
-
-		if (widthWindow > '767' && widthWindow < '992') {
-			widthOffset = 150;
-		} else {
-			widthOffset = 50;
-		}
-
-		mainMediumImage.each(function (idx, item) {
-			$(item).css({
-				right: -(offsetMediumEl - widthOffset)
-			});
-		});
-	} else {
-		mainMediumImage.removeAttr("style");
-	}
-
-	// OTHER IMAGE
-	if (widthWindow > '290' && widthWindow < WINWIDTH_LARGE) {
-
-		imgLeft.each(function (idx, item) {
-			$(item).css({
-				left: -offsetEl
-			});
-		});
-
-		imgRight.each(function (idx, item) {
-			$(item).css({
-				right: -offsetEl
-			});
-		});
-
-		imgLeftSmooth.each(function (idx, item) {
-			$(item).css({
-				left: arrLeftSmoothImage[idx] - offsetEl
-			});
-		});
-
-		imgRightSmooth.each(function (idx, item) {
-			$(item).css({
-				left: arrRightSmoothImage[idx] - offsetEl
-			});
-		});
-	} else {
-		imgLeft.removeAttr("style");
-		imgRight.removeAttr("style");
-		imgLeftSmooth.removeAttr("style");
-		imgRightSmooth.removeAttr("style");
-	}
-});
-/**
- *
- */
 $(function () {
-	var allImage = $(".mainLarge_right, .mainMedium_right, .resize_right, .resize_left, .resize_left-s, .resize_right-s");
+    // Fade In image, fixed jump in load page
+    var allImage = $(".mainLarge_right, .mainMedium_right, .resize_right, .resize_left, .resize_left-s, .resize_right-s");
 
-	allImage.addClass("visible");
+    allImage.addClass("visible");
+
+    /*
+    *
+    * */
+    var arrLeftSmoothImage = [],
+        arrRightSmoothImage = [],
+        imgLeftSmooth = $('.resize_left-s'),
+        imgRightSmooth = $('.resize_right-s');
+
+    pushOffsetValue(imgLeftSmooth, arrLeftSmoothImage);
+    pushOffsetValue(imgRightSmooth, arrRightSmoothImage);
+
+    listenerWindowModify(arrLeftSmoothImage, arrRightSmoothImage);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -13,104 +13,127 @@ function pushOffsetValue(arrElem, arr) {
 	});
 }
 
-/**
- *
- */
-$(window).on("change resize load", function() {
-	let arrLeftSmoothImage  = [],
-		arrRightSmoothImage = [];
-	
-	const mainLargeImage= $(".mainLarge_right"),
-		mainMediumImage = $(".mainMedium_right"),
-		imgRight        = $(".resize_right"),
-		imgLeft         = $(".resize_left"),
-		imgLeftSmooth   = $('.resize_left-s'),
-		imgRightSmooth  = $('.resize_right-s');
-	
-	pushOffsetValue(imgLeftSmooth, arrLeftSmoothImage);
-	pushOffsetValue(imgRightSmooth, arrRightSmoothImage);
-	
-	const WINWIDTH_LARGE    = 1920,
-		WINWIDTH_MEDIUM     = 1366;
-	
-	let widthWindow = $(this).width(),
-		offsetEl    = ((WINWIDTH_LARGE - widthWindow) / 2),
-		offsetMediumEl = ((WINWIDTH_MEDIUM - widthWindow) / 2);
 
-	// BIG DEVICE
-	if((widthWindow > '1348') && (widthWindow < WINWIDTH_LARGE)) {
-		
-		mainLargeImage.each(function(idx, item) {
-			$(item).css({
-				right: -(offsetEl)
-			});
-		});
-		
-	} else {
-		mainLargeImage.removeAttr("style");
-	}
-	
-	// MEDIUM DEVICE
-	if ((widthWindow > '767') && (widthWindow < WINWIDTH_MEDIUM)) {
-		let widthOffset = 0;
-		
-		if(widthWindow > '767' && widthWindow < '992') {
-			widthOffset = 150;
-		} else {
-			widthOffset = 50;
-		}
-		
-		mainMediumImage.each(function(idx, item) {
-			$(item).css({
-				right: -(offsetMediumEl - widthOffset)
-			});
-		});
-		
-	} else {
-		mainMediumImage.removeAttr("style");
-	}
-	
-	// OTHER IMAGE
-	if ((widthWindow > '290') && (widthWindow < WINWIDTH_LARGE)) {
-		
-		imgLeft.each(function(idx, item) {
-			$(item).css({
-				left: -(offsetEl)
-			});
-		});
-		
-		imgRight.each(function(idx, item) {
-			$(item).css({
-				right: -(offsetEl)
-			});
-		});
-		
-		imgLeftSmooth.each(function(idx, item) {
-			$(item).css({
-				left: arrLeftSmoothImage[idx] - offsetEl
-			});
-		});
-		
-		imgRightSmooth.each(function(idx, item) {
-			$(item).css({
-				left: arrRightSmoothImage[idx] - offsetEl,
-			});
-		});
-		
-	} else {
-		imgLeft.removeAttr("style");
-		imgRight.removeAttr("style");
-		imgLeftSmooth.removeAttr("style");
-		imgRightSmooth.removeAttr("style");
-	}
-	
-	
-});
+/**
+ * @name listenerWindowModify
+ * @function
+ *
+ * @param arrLeftImg
+ * @param arrRightImg
+ *
+ * @description
+ */
+function listenerWindowModify(arrLeftImg, arrRightImg) {
+    $(window).on("change resize load", function() {
+        const mainLargeImage= $(".mainLarge_right"),
+            mainMediumImage = $(".mainMedium_right"),
+            imgRight        = $(".resize_right"),
+            imgLeft         = $(".resize_left"),
+            imgLeftSmooth   = $('.resize_left-s'),
+            imgRightSmooth  = $('.resize_right-s');
+
+
+        const WINWIDTH_LARGE    = 1920,
+            WINWIDTH_MEDIUM     = 1366;
+
+        let widthWindow = $(this).width(),
+            offsetEl    = ((WINWIDTH_LARGE - widthWindow) / 2),
+            offsetMediumEl = ((WINWIDTH_MEDIUM - widthWindow) / 2);
+
+        console.log("widthWindow: ", widthWindow);
+
+        // BIG DEVICE
+        if((widthWindow > '1279') && (widthWindow < WINWIDTH_LARGE)) {
+
+            mainLargeImage.each(function(idx, item) {
+                $(item).css({
+                    right: -(offsetEl)
+                });
+            });
+
+        } else {
+            mainLargeImage.removeAttr("style");
+        }
+
+        // MEDIUM DEVICE
+        if ((widthWindow > '767') && (widthWindow < WINWIDTH_MEDIUM)) {
+            let widthOffset = 0;
+
+            if(widthWindow > '767' && widthWindow < '992') {
+                widthOffset = 150;
+            } else {
+                widthOffset = 50;
+            }
+
+            mainMediumImage.each(function(idx, item) {
+                $(item).css({
+                    right: -(offsetMediumEl - widthOffset)
+                });
+            });
+
+        } else {
+            mainMediumImage.removeAttr("style");
+        }
+
+        // OTHER IMAGE
+        if ((widthWindow > '290') && (widthWindow < WINWIDTH_LARGE)) {
+
+            imgLeft.each(function(idx, item) {
+                $(item).css({
+                    left: -(offsetEl)
+                });
+            });
+
+            imgRight.each(function(idx, item) {
+                $(item).css({
+                    right: -(offsetEl)
+                });
+            });
+
+            imgLeftSmooth.each(function(idx, item) {
+                $(item).css({
+                    left: arrLeftImg[idx] - offsetEl
+                });
+            });
+
+            imgRightSmooth.each(function(idx, item) {
+                $(item).css({
+                    left: arrRightImg[idx] - offsetEl,
+                });
+            });
+
+        } else {
+            imgLeft.removeAttr("style");
+            imgRight.removeAttr("style");
+            imgLeftSmooth.removeAttr("style");
+            imgRightSmooth.removeAttr("style");
+        }
+
+
+    });
+}
+
+
 /**
  *
  */
 $(function() {
+	// Fade In image, fixed jump in load page
 	const allImage = $(".mainLarge_right, .mainMedium_right, .resize_right, .resize_left, .resize_left-s, .resize_right-s")
 	
 	allImage.addClass("visible");
+
+
+	/*
+	*
+	* */
+    let arrLeftSmoothImage  = [],
+        arrRightSmoothImage = [],
+        imgLeftSmooth   	= $('.resize_left-s'),
+        imgRightSmooth  	= $('.resize_right-s');
+
+    pushOffsetValue(imgLeftSmooth, arrLeftSmoothImage);
+    pushOffsetValue(imgRightSmooth, arrRightSmoothImage);
+
+    listenerWindowModify(arrLeftSmoothImage, arrRightSmoothImage)
 });
